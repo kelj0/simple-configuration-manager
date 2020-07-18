0d08869874f752f9230713a67eb2a3acc191517d
kelj0 <kegljevickarlo@gmail.com>
2020-07-18 18:44:31 +0200

    update agent

--- agents/agent.sh
+++ agents/agent.sh
@@ -1,38 +1,49 @@
-ROOT_URL = ""
-HB_URL = $ROOT_URL + ""
-IP = $(hostname -I)
+ROOT_URL='localhost';
+HEARTHBEAT_URL=$ROOT_URL+"/api/hearthbeat";
+DOWNLOAD_FOLDER="$HOME";
+CONFIG_NAME="config";
+CONFIG_EXTENSION="zip";
+IP=$(hostname -I);
 
 
 hearthbeat(){
     # ping server on url 
-    echo "Pinging server.."
+    curl -H 'Content-Type:application/json' $HEARTHBEAT_URL -d "{'ip': '$IP'}";
 }
 
 zip_config_files(){
     #  gets config files and compresses it in $HOME directory
-    echo "Get config files.."
-    echo "Compress"
+    zip $HOME/$CONFIG_NAME.$CONFIG_EXTENSION -r /etc/nginx/ /etc/apache2/*.conf /etc/httpd/*.conf /etc/httpd/conf/*.conf /etc/http /etc/ssh/ -x /etc/ssh/*key*
 }
 
 get_config_sha1(){
     # returns sha1 of compressed configs 
-    echo "Pinging server.."
+    return sha1sum $DOWNLOAD_FOLDER/$CONFIG_NAME.$CONFIG_EXTENSION
 }
 
 check_for_new_config(){
     # checks if get_config_sha1 is equal to currently used config on server
-    echo "Checking config.."    
+    # if [[ $(curl -s -o /dev/null -w "%{http_code}" newconfig.com) == 200 ]]; then
+    #   get_and_setup_config;   
+    # endif
+    return;
 }
 
 get_and_setup_config(){
     # downloads a new config, uncompresses and moves to needed directories
-    echo "Downloading new config.."
+    #curl config -o config.zip;
+    # zip -e
+    # mv -r nginx /etc
+    # mv -r apache /etc
+    return;
 }
 
 restart_services(){
     # restart nginx and apache
     sudo systemctl restart nginx.service
     sudo systemctl restart apache2.service
+    sudo systemctl restart sshd.service
+    sudo systemctl restart ssh.service
 }
 
 help(){
@@ -40,4 +51,20 @@ help(){
     echo "Printing help.."
 }
 
-
+case $1 in
+    -hb|--heathbeat)
+        echo 'pinging server..'
+        #hearthbeat;
+        ;;
+    -c|--newconfig)
+        echo 'checking new config..'
+        #check_for_new_config;
+        ;;
+    -r|--restart)
+        echo 'restarting services..'
+        #restart_services;
+        ;;
+    *)
+        help;
+        ;;
+esac
