40e04934dc3b0cfac150f3515340d0a8ca863a26
Marino Skalnik <marino.skalnik@gmail.com>
2020-07-26 19:42:05 +0200

    Add more server info to the mobile app

--- mobile_app/app/src/main/java/com/mskalnik/simpleconfigurationmanager/ServerActivity.kt
+++ mobile_app/app/src/main/java/com/mskalnik/simpleconfigurationmanager/ServerActivity.kt
@@ -6,10 +6,12 @@ import android.widget.TextView
 class ServerActivity : BaseActivity() {
 
     companion object {
-        const val SERVER_NAME_KEY   = "SERVER_NAME";
-        const val SERVER_STATUS_KEY = "SERVER_STATUS";
-        const val SERVER_IP_KEY     = "SERVER_IP";
-        const val SERVER_OS_KEY     = "SERVER_OS";
+        const val SERVER_ID_KEY         = "SERVER_ID";
+        const val SERVER_NAME_KEY       = "SERVER_NAME";
+        const val SERVER_STATUS_KEY     = "SERVER_STATUS";
+        const val SERVER_IP_KEY         = "SERVER_IP";
+        const val SERVER_DELETED_KEY    = "SERVER_DELETED";
+        const val SERVER_OS_KEY         = "SERVER_OS";
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +21,18 @@ class ServerActivity : BaseActivity() {
     }
 
     private fun initValues() {
-        val twServerName: TextView      = findViewById(R.id.twServerNameValue)
-        val twServerStatus: TextView    = findViewById(R.id.twServerStatusValue)
-        val twServerIpAddress: TextView = findViewById(R.id.twServerIpAddressValue)
-        val twServerOsLabel: TextView   = findViewById(R.id.twServerOsValue)
+        val twServerIdValue: TextView       = findViewById(R.id.twServerIdValue)
+        val twServerName: TextView          = findViewById(R.id.twServerNameValue)
+        val twServerStatus: TextView        = findViewById(R.id.twServerStatusValue)
+        val twServerIpAddress: TextView     = findViewById(R.id.twServerIpAddressValue)
+        val twServerDeletedValue: TextView  = findViewById(R.id.twServerDeletedValue)
+        val twServerOsLabel: TextView       = findViewById(R.id.twServerOsValue)
 
-        twServerName.text       = intent.getStringExtra(SERVER_NAME_KEY)
-        twServerStatus.text     = if (intent.getBooleanExtra(SERVER_STATUS_KEY, false)) "ONLINE" else "OFFLINE"
-        twServerIpAddress.text  = intent.getStringExtra(SERVER_IP_KEY)
-        twServerOsLabel.text    = intent.getStringExtra(SERVER_OS_KEY)
+        twServerIdValue.text        = intent.getIntExtra(SERVER_ID_KEY, 0).toString()
+        twServerName.text           = intent.getStringExtra(SERVER_NAME_KEY)
+        twServerStatus.text         = if (intent.getBooleanExtra(SERVER_STATUS_KEY, false)) "ONLINE" else "OFFLINE"
+        twServerIpAddress.text      = intent.getStringExtra(SERVER_IP_KEY)
+        twServerDeletedValue.text   = if (intent.getBooleanExtra(SERVER_DELETED_KEY, false)) "Yes" else "No"
+        twServerOsLabel.text        = intent.getStringExtra(SERVER_OS_KEY)
     }
 }
--- mobile_app/app/src/main/java/com/mskalnik/simpleconfigurationmanager/adapter/ServerListViewHolder.kt
+++ mobile_app/app/src/main/java/com/mskalnik/simpleconfigurationmanager/adapter/ServerListViewHolder.kt
@@ -6,6 +6,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mskalnik.simpleconfigurationmanager.R
 import com.mskalnik.simpleconfigurationmanager.ServerActivity
+import com.mskalnik.simpleconfigurationmanager.ServerActivity.Companion.SERVER_DELETED_KEY
+import com.mskalnik.simpleconfigurationmanager.ServerActivity.Companion.SERVER_ID_KEY
 import com.mskalnik.simpleconfigurationmanager.ServerActivity.Companion.SERVER_IP_KEY
 import com.mskalnik.simpleconfigurationmanager.ServerActivity.Companion.SERVER_NAME_KEY
 import com.mskalnik.simpleconfigurationmanager.ServerActivity.Companion.SERVER_OS_KEY
@@ -22,9 +24,11 @@ class ServerListViewHolder(view: View, servers: List<Server>) : RecyclerView.Vie
         view.setOnClickListener{
             val intent = Intent(view.context, ServerActivity::class.java)
 
+            intent.putExtra(SERVER_ID_KEY, servers[adapterPosition].idServer)
             intent.putExtra(SERVER_NAME_KEY, servers[adapterPosition].serverName)
             intent.putExtra(SERVER_STATUS_KEY, servers[adapterPosition].isOnline)
             intent.putExtra(SERVER_IP_KEY, servers[adapterPosition].ipAddress)
+            intent.putExtra(SERVER_DELETED_KEY, servers[adapterPosition].deleted)
             intent.putExtra(SERVER_OS_KEY, servers[adapterPosition].operatingSystem)
 
             view.context.startActivity(intent);
